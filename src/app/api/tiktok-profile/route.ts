9ac2edaaf89800604c95c6fdf8992bf81fc63f13
Inopem Adika <49522880+npdkdev@users.noreply.github.com>
2024-08-09 18:02:01 +0700

    upload local

---
+++ src/app/api/tiktok-profile/route.ts
@@ -0,0 +1,60 @@
+import { NextRequest, NextResponse } from "next/server";
+import axios from "axios";
+import * as cheerio from "cheerio";
+import {data} from "cheerio/lib/api/attributes";
+
+export async function GET(request: NextRequest) {
+  const username = request.nextUrl.searchParams.get("username");
+
+  if (!username) {
+    return NextResponse.json(
+      { message: "Username is required" },
+      { status: 400 }
+    );
+  }
+
+  try {
+    const profile = await getTiktokProfile(username);
+    return NextResponse.json(profile);
+  } catch (error) {
+    console.error("Error fetching Tiktok profile:", error);
+    return NextResponse.json(
+      { message: "Error fetching Tiktok profile" },
+      { status: 500 }
+    );
+  }
+}
+
+async function getTiktokProfile(username: string) {
+
+  const profileUrl = `https://www.tiktok.com/@${username.replace(/@/g, '')}/`;
+  const response = await axios.get(profileUrl);
+  const $ = cheerio.load(response.data);
+
+  const profile: any = {};
+  const jsonString = $('#__UNIVERSAL_DATA_FOR_REHYDRATION__').html();
+
+  // Parse the JSON string into a JavaScript object
+  const jsonData = JSON.parse(jsonString);
+  const dataJson = jsonData.__DEFAULT_SCOPE__
+  const userDetail = dataJson['webapp.user-detail'].userInfo;
+  if (!userDetail) {
+    throw new Error('Pengguna tidak ditemukan')
+  }
+  profile.name = userDetail['user']['nickname']
+  profile.username = userDetail['user']['uniqueId']
+  profile.bio = userDetail['user']['signature']
+  profile.created_at = userDetail['user']['createdTime']
+  profile.avatar = userDetail['user']['avatarMedium']
+  profile.verified = userDetail['user']['verified']
+  profile.is_private = userDetail['user']['privateAccount']
+  profile.last_modified_name = userDetail['user']['nickNameModifyTime']
+  // profile.stats = userDetail['stats']
+  profile.followers = userDetail['stats'].followerCount
+  profile.following = userDetail['stats'].followingCount
+  profile.likes = userDetail['stats'].heart
+  profile.videos = userDetail['stats'].videoCount
+  profile.friends = userDetail['stats'].friendCount
+  return profile;
+}
+
---
+++ src/app/page.tsx
@@ -0,0 +1,27 @@
+import RoastForm from "@/components/RoastForm";
+
+export default function Home() {
+  return (
+    <main className="bg-gray-200 min-h-screen flex flex-col">
+      <div className="flex-grow flex flex-col justify-center">
+        <h1 className="text-3xl font-bold text-center mb-8">
+          Tiktok Roaster
+        </h1>
+        <RoastForm />
+      </div>
+      <footer className="text-center m-8">
+        <p>
+          Developed by{" "}
+          <a
+            href="https://www.instagram.com/nppddkk"
+            className="text-blue-500"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            @nppddkk
+          </a>
+        </p>
+      </footer>
+    </main>
+  );
+}
